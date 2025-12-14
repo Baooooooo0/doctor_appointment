9316adf5a6257b5a73802fc99bb6c3a59a4362a1
Baooooooo0 <giabao4785@gmail.com>
2025-12-14 15:15:15 +0700

    update and comment code

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -1,23 +1,36 @@
-const pool = require('../config/db');
-const { v4: uuid } = require('uuid');
+const pool = require('../config/db'); //connection pool MySQL
+const { v4: uuid } = require('uuid'); //tạo id duy nhất cho appointment
 
+//lay du lieu tu req 
 exports.createAppointment = async (req, res) => {
-  const { doctorId, scheduleId } = req.body;
-  const patientId = req.user.patientId;
+  const { doctorId, scheduleId } = req.body; //lay doctorId va scheduleId tu frontend
+  const patientId = req.user.patientId; //lay patientId tu JWT middleware
 
+  //lay connection rieng tu pool de chuan bi dung transaction, khong dung connection chung
   const conn = await pool.getConnection();
   try {
-    await conn.beginTransaction();
+    //bat dau transaction
+    //Từ đây trở đi:
+      // Mọi query chưa ghi hẳn vào DB
+      // Chỉ khi commit() thì mới lưu
+      // Có lỗi → rollback() → DB quay lại trạng thái cũ
+    await conn.beginTransaction(); 
 
-    // lock schedule
+    //Tìm schedule theo scheduleId -> Chỉ lấy schedule còn trống (is_available = TRUE) -> FOR UPDATE → khóa row này
+    // FOR UPDATE: Request khác không thể đọc/ghi row này cho tới khi transaction hiện tại kết thúc -> Tránh 2 người đặt cùng 1 lịch
     const [schedules] = await conn.query(
       'SELECT * FROM schedules WHERE id = ? AND is_available = TRUE FOR UPDATE',
       [scheduleId]
     );
-    if (!schedules.length) throw 'Schedule not available';
 
-    const schedule = schedules[0];
+    //Kiểm tra schedule có hợp lệ không
+    //Không tìm thấy schedule -> Ném lỗi → nhảy xuống catch → rollback
+    if (!schedules.length) throw new Error('Schedule not available');
 
+    //Lấy dữ liệu schedule
+    const schedule = schedules[0]; //Vì id là unique, Lấy phần tử đầu tiên
+
+    //Tạo appointment mới
     await conn.query(
       `INSERT INTO appointments 
       (id, patient_id, doctor_id, schedule_id, date, start_time, end_time)
@@ -33,17 +46,19 @@ exports.createAppointment = async (req, res) => {
       ]
     );
 
+    //Đánh dấu schedule đã bị đặt
     await conn.query(
       'UPDATE schedules SET is_available = FALSE WHERE id = ?',
       [scheduleId]
     );
 
+    //Insert appointment & Update schedule, Nếu 1 trong 2 fail → không có gì được ghi
     await conn.commit();
     res.json({ message: 'Appointment created' });
   } catch (err) {
-    await conn.rollback();
-    res.status(400).json({ error: err });
+    await conn.rollback(); //Nếu có lỗi (Schedule không available, Lỗi SQL, ...) → rollback
+    res.status(400).json({ error: err }); //request không hợp lệ
   } finally {
-    conn.release();
+    conn.release();//giải phóng connection
   }
 };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,32 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (roles = []) => {
+  //middleware bat req
   return (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.sendStatus(401);
+    //Lấy JWT token từ header Authorization theo chuẩn Bearer
+    const authHeader = req.headers.authorization;
 
+    //neu header authorization hop le thi lay token neu khong thi tra ve 401
+    if (!authHeader) return res.sendStatus(401);
+
+    //"Bearer eyJhbGciOiJIUzI1NiIs...".split(' ') -> ["Bearer", "eyJhbGciOiJIUzI1NiIs..."] -> ["0", "1"] -> lay 1
+    const token = authHeader.split(' ')[1];
+
+    //try catch bat loi token hoac secret
     try {
+      //kiem tra token co hop le khong (dung chu ky?, con han?) va giai ma token (decoded)
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //kiem tra role.length va middleware co yeu cau role khong?
+      // auth(['admin']) -> roles.length > 0 hoac auth() -> roles.length === 0
+      //roles = ['admin'] nhung decoded.role = 'user'
       if (roles.length && !roles.includes(decoded.role)) {
-        return res.sendStatus(403);
+        return res.sendStatus(403); //tra 403 vi user da dang nhap (token hop le) nhung khong du quyen
       }
-      req.user = decoded;
-      next();
+      req.user = decoded; //gan user vao req de controller khong can decoded lai token 
+      next(); //neu token hop le va role hope le thi vao controller
     } catch {
-      res.sendStatus(401);
+      res.sendStatus(401); // tra ve unauthorize coi nhu chua dang nhap 
     }
   };
 };
