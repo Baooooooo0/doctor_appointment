0b6ebff7677f90f67b0fd5262b68f1c527410c62
Baooooooo0 <giabao4785@gmail.com>
2025-12-14 17:45:09 +0700

    update and comment code

--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');//cho phép frontend gọi API từ domain khác
 
+//Tạo instance của Express
+// Tất cả request sẽ đi qua app
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+//Middleware dùng chung (chạy cho MỌI request)
+//Cho phép: Frontend ở domain khác gọi API
+//Tránh lỗi: Blocked by CORS policy
+app.use(cors());
+
+//Parse JSON body
+app.use(express.json()); //Chuyển: { "email": "a@gmail.com" } thành: req.body.email
+
+//Gắn các router (định tuyến API)
+//Auth API: Mọi route trong auth.routes.js sẽ có prefix:/api/v1/auth/...
 app.use('/api/v1/auth', require('./routes/auth.routes'));
+
+//Patients API
 app.use('/api/v1/patients', require('./routes/patient.routes'));
+
+//Doctors API
 app.use('/api/v1/doctors', require('./routes/doctor.routes'));
+
+//Appointments API
 app.use('/api/v1/appointments', require('./routes/appointment.routes'));
+
+//Schedules API
 app.use('/api/v1/schedules', require('./routes/schedule.routes'));
+
+//Reviews API
 app.use('/api/v1/reviews', require('./routes/review.routes'));
+
+//Notifications API
 app.use('/api/v1/notifications', require('./routes/notification.routes'));
 
 module.exports = app;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,15 @@
 const pool = require('../config/db');
 
+//nhan vao email va tra ve user hoac undefined
 exports.findByEmail = async (email) => {
-  const [rows] = await pool.query(
+  const [rows] = await pool.query( // rows la mang ket qua thong tin user duoc tra ve
     'SELECT * FROM users WHERE email = ?',
     [email]
   );
-  return rows[0];
+  return rows[0]; // lay user dau tien, Email là unique->Nên chỉ có 0 hoặc 1 record->Nếu không tìm thấy → undefined
 };
 
+//nhan vao object user va insert user vao db
 exports.create = async (user) => {
   await pool.query(
     `INSERT INTO users (id, name, email, password, role)
